d9d6cd18ff92a77a3da830deabe0140a8dd05011
willianlemes <willianlemes94@gmail.com>
2020-08-06 14:28:45 -0300

    Criação da página TeacherList

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import "./styles.css";
+import PageHeader from "../../components/PageHeader";
+
+function TeacherList() {
+  return (
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Estes são os proffys disponíveis.">
+        <form action="" id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Matéria</label>
+            <input type="text" id="subjetc" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week-day">Dias da semana</label>
+            <input type="text" id="week-day" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hora</label>
+            <input type="text" id="time" />
+          </div>
+        </form>
+      </PageHeader>
+    </div>
+  );
+}
+
+export default TeacherList;
